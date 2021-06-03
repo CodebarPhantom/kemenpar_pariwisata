41ab9788a9fc71fae85a093cd07a53f83ebe318c
egostigma <30386767+egostigma@users.noreply.github.com>
2021-06-03 10:47:10 +0700

    test db seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Database\Seeds;
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PrimaryTestsSeeder::class);
+        $this->call('PrimaryTestsSeeder');
     }
 }
