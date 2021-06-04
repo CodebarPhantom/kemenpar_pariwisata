bae38678dc6dce20e8611c1f60c10938adfae40d
egostigma <30386767+egostigma@users.noreply.github.com>
2021-06-04 18:58:06 +0700

    feat: API logged in user data

--- app/Http/Controllers/API/Auth/LoginController.php
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -37,59 +37,75 @@ class LoginController extends Controller
     {
         // testing eloquent relationship
 
-        // return User::select('users.*')
-        //     ->where('id', auth()->user()->id)
-        //     ->with('tourism_info.categories')
-        //     ->with('tourism_info.tickets.items')
-        //     // ->with('primary_tests.secondary_tests')
-        //     ->with('tourism_info.promotion')
-        //     ->with('user_activity_logs')
-        //     ->get();
+        $result = [
+            'data' => User::select('users.*')
+                ->where('id', auth()->user()->id)
+                ->with('tourism_info.categories')
+                // ->with('tourism_info.tickets.items')
+                // ->with('primary_tests.secondary_tests')
+                ->with('tourism_info.promotion')
+                // ->with('user_activity_logs')
+                ->get(),
+        ];
 
-        // return UserActivityLog::select('user_activity_logs.*')
-        //     ->leftJoin('users as u', 'u.id', '=', 'user_activity_logs.user_id')
-        //     ->where('u.id', auth()->user()->id)
-        //     ->with('user')
-        //     ->get();
+        // $result = [
+        //     'data' => UserActivityLog::select('user_activity_logs.*')
+        //         ->leftJoin('users as u', 'u.id', '=', 'user_activity_logs.user_id')
+        //         ->where('u.id', auth()->user()->id)
+        //         ->with('user')
+        //         ->get(),
+        // ];
 
-        // return TourismInfo::select('tourism_infos.*')
-        //     ->leftjoin('users as u', 'u.tourism_info_id', '=', 'tourism_infos.id')
-        //     ->where('u.id', auth()->user()->id)
-        //     ->with('categories')
-        //     ->with('tickets.items')
-        //     ->with('promotion')
-        //     ->get();
+        // $result = [
+        //     'data' => TourismInfo::select('tourism_infos.*')
+        //         ->leftjoin('users as u', 'u.tourism_info_id', '=', 'tourism_infos.id')
+        //         ->where('u.id', auth()->user()->id)
+        //         ->with('categories')
+        //         ->with('tickets.items')
+        //         ->with('promotion')
+        //         ->get(),
+        // ];
 
-        // return TourismInfoCategories::select('tourism_info_categories.*')
-        //     ->leftjoin('tourism_infos as i', 'i.id', '=', 'tourism_info_categories.tourism_info_id')
-        //     ->leftjoin('users as u', 'u.tourism_info_id', '=', 'i.id')
-        //     ->where('u.id', auth()->user()->id)
-        //     ->with('tourism_info.categories')
-        //     ->with('tourism_info.tickets.items')
-        //     ->with('tourism_info.promotion')
-        //     ->get();
+        // $result = [
+        //     'data' => TourismInfoCategories::select('tourism_info_categories.*')
+        //         ->leftjoin('tourism_infos as i', 'i.id', '=', 'tourism_info_categories.tourism_info_id')
+        //         ->leftjoin('users as u', 'u.tourism_info_id', '=', 'i.id')
+        //         ->where('u.id', auth()->user()->id)
+        //         ->with('tourism_info.categories')
+        //         ->with('tourism_info.tickets.items')
+        //         ->with('tourism_info.promotion')
+        //         ->get(),
+        // ];
 
-        // return Ticket::select('tickets.*')
-        //     ->leftjoin('users as u', 'u.id', '=', 'tickets.user_id')
-        //     ->where('u.id', auth()->user()->id)
-        //     ->with('items')
-        //     ->with('tourism_info.categories')
-        //     ->with('promotion')
-        //     ->get();
+        // $result = [
+        //     'data' => Ticket::select('tickets.*')
+        //         ->leftjoin('users as u', 'u.id', '=', 'tickets.user_id')
+        //         ->where('u.id', auth()->user()->id)
+        //         ->with('items')
+        //         ->with('tourism_info.categories')
+        //         ->with('promotion')
+        //         ->get(),
+        // ];
 
-        // return TicketItems::select('ticket_items.*')
+        // $result = [
+        //     'data' => TicketItems::select('ticket_items.*')
         //     ->leftjoin('tickets as t', 't.id', '=', 'ticket_items.ticket_id')
         //     ->where('t.user_id', auth()->user()->id)
         //     ->with('ticket')
         //     ->with('categories')
-        //     ->get();
+        //     ->get(),
+        // ];
 
-        // return TicketPromotion::select('ticket_promotions.*')
+        // $result = [
+        //     'data' => TicketPromotion::select('ticket_promotions.*')
         //     ->leftjoin('tickets as t', 't.ticket_promotion_id', '=', 'ticket_promotions.id')
         //     ->leftjoin('users as u', 'u.id', '=', 't.user_id')
         //     ->where('u.id', auth()->user()->id)
         //     ->with('tourism_info.categories')
         //     ->with('tickets')
-        //     ->get();
+        //     ->get(),
+        // ];
+
+        return response()->json($result, 200);
     }
 }
