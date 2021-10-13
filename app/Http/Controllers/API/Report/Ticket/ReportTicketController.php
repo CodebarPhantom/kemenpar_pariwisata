7484441df37217af5cc7882b7e50c90bf6abe6b8
Eryan Fauzan <eryangamer10@gmail.com>
2021-10-13 09:50:51 +0700

    fix: get all report

--- app/Http/Controllers/API/Report/Ticket/ReportTicketController.php
+++ app/Http/Controllers/API/Report/Ticket/ReportTicketController.php
@@ -14,18 +14,23 @@ class ReportTicketController extends Controller
         $startDate = date('Y-m-d');
         $endDate = date('Y-m-d');
         $isQr = $request->is_qr;
+        $getAll = $request->get_all ?: false;
 
         $visitorRevenueDaily = TourismInfo::select('tourism_infos.name as tourism_name')
             ->selectRaw('ifnull( cat.NAME, "Umum" ) AS category_name')
             ->selectRaw('ifnull(sum( ti.quantity ), 0) count_visitor')
             ->selectRaw('ifnull(sum( ti.quantity * ti.price ), 0) sum_price')
             ->leftJoin('tourism_info_categories as cat', 'cat.tourism_info_id', '=', 'tourism_infos.id')
-            ->leftJoin('tickets as t', function ($join) use ($startDate, $endDate, $isQr) {
+            ->leftJoin('tickets as t', function ($join) use ($startDate, $endDate, $isQr, $getAll) {
                 $join->on('t.tourism_info_id', '=', 'tourism_infos.id');
                 $join->where('t.created_at', '>=', $startDate . ' 00:00:00');
                 $join->where('t.created_at', '<=', $endDate . ' 23:59:59');
                 $join->where('t.status', 1);
-                $join->where('t.is_qr',$isQr); 
+
+                if(!$getAll){
+                    $join->where('t.is_qr',$isQr);
+                }
+               
             })
             ->leftJoin('ticket_items as ti', function ($join) {
                 $join->on('ti.ticket_id', '=', 't.id');
@@ -53,18 +58,21 @@ class ReportTicketController extends Controller
         $startDate = Carbon::parse($request->start_date)->format('Y-m-d');
         $endDate = Carbon::parse($request->end_date)->format('Y-m-d');
         $isQr = $request->is_qr;
+        $getAll = $request->get_all ?: false;
 
         $visitorRevenueDaily = TourismInfo::select('tourism_infos.name as tourism_name')
             ->selectRaw('ifnull( cat.NAME, "Umum" ) AS category_name')
             ->selectRaw('ifnull(sum( ti.quantity ), 0) count_visitor')
             ->selectRaw('ifnull(sum( ti.quantity * ti.price ), 0) sum_price')
             ->leftJoin('tourism_info_categories as cat', 'cat.tourism_info_id', '=', 'tourism_infos.id')
-            ->leftJoin('tickets as t', function ($join) use ($startDate, $endDate, $isQr) {
+            ->leftJoin('tickets as t', function ($join) use ($startDate, $endDate, $isQr, $getAll) {
                 $join->on('t.tourism_info_id', '=', 'tourism_infos.id');
                 $join->where('t.created_at', '>=', $startDate . ' 00:00:00');
                 $join->where('t.created_at', '<=', $endDate . ' 23:59:59');
                 $join->where('t.status', 1);
-                $join->where('t.is_qr',$isQr);
+                if(!$getAll){
+                    $join->where('t.is_qr',$isQr);
+                }
             })
             ->leftJoin('ticket_items as ti', function ($join) {
                 $join->on('ti.ticket_id', '=', 't.id');
@@ -92,19 +100,21 @@ class ReportTicketController extends Controller
         $startDate = date('Y-m-01');
         $endDate = date('Y-m-t');
         $isQr = $request->is_qr;
-
+        $getAll = $request->get_all ?: false;
 
         $visitorRevenueMonthly = TourismInfo::select('tourism_infos.name as tourism_name')
             ->selectRaw('ifnull( cat.NAME, "Umum" ) AS category_name')
             ->selectRaw('ifnull(sum( ti.quantity ), 0) count_visitor')
             ->selectRaw('ifnull(sum( ti.quantity * ti.price ), 0) sum_price')
             ->leftJoin('tourism_info_categories as cat', 'cat.tourism_info_id', '=', 'tourism_infos.id')
-            ->leftJoin('tickets as t', function ($join) use ($startDate, $endDate, $isQr) {
+            ->leftJoin('tickets as t', function ($join) use ($startDate, $endDate, $isQr, $getAll) {
                 $join->on('t.tourism_info_id', '=', 'tourism_infos.id');
                 $join->where('t.created_at', '>=', $startDate . ' 00:00:00');
                 $join->where('t.created_at', '<=', $endDate . ' 23:59:59');
                 $join->where('t.status', 1);
-                $join->where('t.is_qr', $isQr);
+                if(!$getAll){
+                    $join->where('t.is_qr',$isQr);
+                }
             })
             ->leftJoin('ticket_items as ti', function ($join) {
                 $join->on('ti.ticket_id', '=', 't.id');
